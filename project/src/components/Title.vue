171de7daea9b49c4d5c6d31f52fc8404de2385dd
davide <davide.baiocchi97@gmail.com>
2021-07-05 15:36:24 +0200

    mod

---
+++ project/src/components/Title.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>
+        La mia prima app con CLi!
+    </h1>
+</template>
+
+<script>
+
+export default {
+  name: 'Title'
+}
+</script>
+
+
+<style lang="scss">
+#app {
+  font-size: 20px;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
